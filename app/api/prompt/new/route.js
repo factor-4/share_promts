e2a0cfb11cb1d662da44a127aeef7bbe06b322d4
Shahn Khan <snk.sojib@gmail.com>
2024-02-01 23:56:31 +0200

    up

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -7,7 +7,8 @@ export const POST = async (request) => {
     try {
         await connectToDB();
         const newPrompt = new Prompt({ creator: userId, prompt, tag });
-        console.log('hi check me')
+        console.log('hi check me more')
+        console.log('hi check me more again')
 
         await newPrompt.save();
         return new Response(JSON.stringify(newPrompt), { status: 201 })
